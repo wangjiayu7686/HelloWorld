fbfd5d74475ed086031665d9a54710d328d83927
JIAYU WANG <wangjiayu7686@gmail.com>
2024-03-06 10:19:46 +0900

    hello02

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		
-//		System.out.println("Hello World!");
+		System.out.println("Hello World01!");
+		System.out.println("Hello World02!");
+		System.out.println("Hello World03!");
 		
 		
 		
