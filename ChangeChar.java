7a800ffd1a26eb10c49d77e39b7367a63377d68a
JIAYU WANG <wangjiayu7686@gmail.com>
2024-07-08 22:00:05 +0900

    20240208_wangjiayu

---
+++ ChangeChar.java
@@ -0,0 +1,20 @@
+//转译字符的使用
+public class ChangeChar {
+	
+	public static void main(String args[]) {
+		
+//		制表位，实现对齐\t
+		System.out.println("軽井沢\t御代田");
+		
+//		换行符\n
+		System.out.println("東京\n大阪");
+		
+//		haha\heihei\\huhu
+		System.out.println("haha\\heihei\\\\huhu");
+		
+//		换行
+		System.out.println("ttt\nnnn");
+		
+	}
+
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,13 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		
+//		一个源文件中最多只能有一个public类。其他类个数不限。 public类和所在文件名要一致
+		
+		
 		System.out.println("Hello World01!");
 		System.out.println("Hello World02!");
 		System.out.println("Hello World03!");
+		
 		
 		
 		
