cb0591290b0cdd8b244709e1ec48cdbb4b191af2
JIAYU WANG <wangjiayu7686@gmail.com>
2024-07-09 20:47:03 +0900

    42/910_wangjiayu_20240709

---
+++ page01/IntDetail.java
@@ -0,0 +1,28 @@
+package page01;
+
+public class IntDetail {
+	
+	
+	public static void main(String[] args) {
+		
+//		int a = 3;
+//		
+//		a = 8;
+		
+//		int a = 4;
+		
+//		System.out.println(a);
+		
+//		System.out.println(100 + 98);
+//		System.out.println("100" + 98);
+//		System.out.println(100 + 3 + "hello");
+//		System.out.println("hello" + 100 + 3);
+		
+		int n1 = 1;		//4个字节
+		
+//		int n3 = 1L;	//8个字节，报错
+		long n2 = 1L;
+		
+	}
+
+}
