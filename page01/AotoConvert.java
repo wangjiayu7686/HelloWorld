1a20583f278a71fb2435cc49f515fbe82bc7c9ec
JIAYU WANG <wangjiayu7686@gmail.com>
2024-07-11 21:48:42 +0900

    20240711_wangjiayu_P54

---
+++ page01/AotoConvert.java
@@ -0,0 +1,16 @@
+package page01;
+
+public class AotoConvert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 'a';
+		double d1 = 80;
+		
+		System.out.println(num);	//97
+		System.out.println(d1);		//80.0
+
+	}
+
+}
---
+++ page01/Boolean01.java
@@ -0,0 +1,18 @@
+package page01;
+
+public class Boolean01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean isPass = false;
+		
+		if(isPass == true) {
+			System.out.println("pass");
+		}else {
+			System.out.println("unPass");
+		}
+
+	}
+
+}
