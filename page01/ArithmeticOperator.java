27186cf963314d70ace191e2929fabec8907eb6b
JIAYU WANG <wangjiayu7686@gmail.com>
2024-07-17 18:23:03 +0900

    240717_wangjiayu_P64

---
+++ page01/ArithmeticOperator.java
@@ -0,0 +1,23 @@
+package page01;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(10 / 4);	//2
+		System.out.println(10.0 / 4);	//2.5
+		
+		double d = 10 / 4;
+		System.out.println(d);	//2.0
+		
+		//% 取模 的公式： a % b = a - a / b * b
+		System.out.println(10 % -3);	//1
+		
+		int j = 8;
+		int k = j++;
+		System.out.println(k +" " + j);	
+
+	}
+
+}
